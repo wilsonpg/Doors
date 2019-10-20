f33de2c08b2afb2339598cec53f38229fa52b639
Paul Wilson <54870992+wilsonpg@users.noreply.github.com>
2019-10-19 23:56:21 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+`use strict`;
+
+/* Main idea: Starts on purple screen.  User is searching for a door the same color as the screen.
+Once they open that door, they get some prize.
+
+Commandments:
+1. Make it so that the user cannot possibly find the purple door until ~10 iterations.
+2. Adjust the door colors to align with random css colors chose from an array.
+3. User must click on one of three doors on the page.
+    a. User is then asked a riddle.
+        i. If they answer correctly, they move through the door.
+        ii. If they answer incorrectly, they are barred from passage through that door. (becomes grey / colorless and inaccessible)
+    b. Potential to add more games / puzzles to this as I go.
+*/
