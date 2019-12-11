49d08ec6ab1259499fb1437ae01e25a227d54b7b
Paul Wilson <54870992+wilsonpg@users.noreply.github.com>
2019-12-10 21:53:04 -0500

    handle disconnect

--- app/model/db.js
+++ app/model/db.js
@@ -3,14 +3,30 @@
 var mysql = require(`mysql2`);
 
 //local mysql db connection
-let connection = mysql.createConnection({
-    host: `us-cdbr-iron-east-05.cleardb.net`,
-    port: 3306,
-    user: `b4d1a887c32dc7`,
-    password: `f258bcbe`,
-    database: `heroku_34a823f031d8525`
-});
-
-connection.connect();
+function handleDisconnect() {
+    connection = mysql.createConnection({
+        host: `us-cdbr-iron-east-05.cleardb.net`,
+        port: 3306,
+        user: `b4d1a887c32dc7`,
+        password: `f258bcbe`,
+        database: `heroku_34a823f031d8525`
+    }); 
+                                                    
+    connection.connect(function(err) {             
+      if(err) {                                     
+        setTimeout(handleDisconnect, 2000); 
+      }                                     
+    });                                    
+                                            
+    connection.on(`error`, function(err) {
+      if(err.code === `PROTOCOL_CONNECTION_LOST`) {
+        handleDisconnect();                         
+      } else {                                     
+        throw err;                                 
+      }
+    });
+  }
+  
+  handleDisconnect();
 
 module.exports = connection;
