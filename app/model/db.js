f6561f0d6328267166f96b20c213fe8d210d622e
Paul Wilson <54870992+wilsonpg@users.noreply.github.com>
2019-12-10 22:39:42 -0500

    introducing connection pooling

--- app/model/db.js
+++ app/model/db.js
@@ -2,32 +2,17 @@
 
 var mysql = require(`mysql2`);
 
-//local mysql db connection
-function handleDisconnect() {
-    connection = mysql.createConnection({
-        host: `us-cdbr-iron-east-05.cleardb.net`,
-        port: 3306,
-        user: `b4d1a887c32dc7`,
-        password: `f258bcbe`,
-        database: `heroku_34a823f031d8525`
-    }); 
-                                                    
-    connection.connect(function(err) {             
-      if(err) {                                     
-        setTimeout(handleDisconnect); 
-      }                                     
-    });                                    
-                                            
-    connection.on(`error`, function(err) {
-        console.log(err.code);
-      if(err.code === `PROTOCOL_CONNECTION_LOST`) {
-        handleDisconnect();                         
-      } else {                                     
-        throw err;                                 
-      }
-    });
-  }
-  
-  handleDisconnect();
+//local mysql db connection pool
+const pool = mysql.createPool({
+    host: `us-cdbr-iron-east-05.cleardb.net`,
+    port: 3306,
+    user: `b4d1a887c32dc7`,
+    password: `f258bcbe`,
+    database: `heroku_34a823f031d8525`,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });            
+const promisePool = pool.promise();
 
-module.exports = connection;
+module.exports = promisePool;
